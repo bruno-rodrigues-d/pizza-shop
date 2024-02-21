a87531bbc6c7812164c58864e38766e7d7f4c0e5
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2024-02-21 17:13:26 -0300

    feat: initial configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
   return (
     <>
       <div>
         <h1>Hello world</h1>
+        <Button>Teste</Button>
       </div>
     </>
   )
